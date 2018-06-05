4909d857e9523ef304be43edba8f633d802f984e
karthisanjaykk <karthikoms10>
2018-06-05 15:06:43 +0530

    karthikoms

---
+++ src/com/karthi/org/ControllingFlow.java
@@ -0,0 +1,14 @@
+package com.karthi.org;
+
+public class ControllingFlow {
+	public static void main(String[] args) {
+		int one =20;
+	boolean three = one ==12 ? true : false;
+	System.out.println(three);
+	}
+	
+	}
+	
+		
+
+
---
+++ src/com/karthi/org/Switch.java
@@ -0,0 +1,17 @@
+package com.karthi.org;
+
+public class Switch {
+	public static void main(String[] args) {
+		int kk=23;
+		switch (kk){
+		case 23:
+			System.out.println("kathi");
+		case 12:
+			System.out.println("keyan");
+		
+		}
+		
+
+	}
+
+}
